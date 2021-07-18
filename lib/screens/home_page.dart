66f53e1a7c0042373ef957e8bfe1f025977b89f5
jonntan <67737040+jonntan@users.noreply.github.com>
2021-07-19 02:49:16 +0800

    Added new screens

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:todoapp/widgets/landing_page_widget.dart';
+import 'package:todoapp/widgets/sign_in_google.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("Something went wrong!"),
+              );
+            } else if (snapshot.hasData) {
+              return LandingPageWidget();
+            } else {
+              return SignInGoogleWidget();
+            }
+          },
+        ),
+      );
+}
