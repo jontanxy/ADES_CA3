824dd8b0c76c5bd911986c399a7f768edae1c879
jonntan <67737040+jonntan@users.noreply.github.com>
2021-07-20 05:41:41 +0800

    Updated color palette

---
+++ lib/utilities/color_palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/painting.dart';
+
+class ColorPalette {
+  static const Color scienceBlue = Color(0xff0770df);
+  static const Color neonBlue = Color(0xff1eb9e5);
+  static const Color wisteriaPurple = Color(0xff9e60ac);
+  static const Color delugePurple = Color(0xffdb7a8c);
+  static const Color lightBrown = Color(0xffe0ae86);
+  static const Color tumbleweedBrown = Color(0xffdc9e87);
+}
