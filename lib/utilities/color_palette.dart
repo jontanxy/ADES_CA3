a0fe76e26010b42acd738211d120fdf5d8cb20bd
jonntan <67737040+jonntan@users.noreply.github.com>
2021-07-29 04:04:46 +0800

    Added new colors

--- lib/utilities/color_palette.dart
+++ lib/utilities/color_palette.dart
@@ -7,4 +7,14 @@ class ColorPalette {
   static const Color delugePurple = Color(0xffdb7a8c);
   static const Color lightBrown = Color(0xffe0ae86);
   static const Color tumbleweedBrown = Color(0xffdc9e87);
+  static const Color facebookBlue = Color(0xff4267B2);
+  static const Color pastelBlue = Color(0xffadd8e6);
+  static const Color lightRed = Color(0xffeb5951);
+}
+
+class GradientColorPalette {
+  static const Color royalPurple = Color(0xff804fb3);
+  static const Color amethystPurple = Color(0xff9969c7);
+  static const Color purpleHeart = Color(0xff6a359c);
+  static const Color lenurplePurple = Color(0xffb589d6);
 }
